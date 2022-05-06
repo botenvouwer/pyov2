11cf258018147f2b809194beb373e3aa7907f6b1
wloosma2 <william.loosman@enexis.nl>
2022-05-06 08:06:40 +0200

    initial commit

---
+++ ov2.py
@@ -0,0 +1,25 @@
+import csv
+import struct
+
+STATUS_DELETED = 0
+# todo: not implemented -> see https://gordthompson.github.io/ov2optimizer/ov2FileFormat.html for more detail
+# STATUS_SKIPPER = 1
+STATUS_REGULAR = 2
+# STATUS_EXTENDED = 3
+
+
+def to_ov2(lon, lat, label, status=STATUS_REGULAR):
+    size = 14 + len(label)
+    lon = int(lon * 100000)
+    lat = int(lat * 100000)
+    label = label.encode('utf8')
+    buff = struct.pack(f'<B3i{len(label) + 1}s', status, size, lon, lat, label)
+    return buff
+
+
+def from_ov2(buff):
+    status, size, lon, lat, label = struct.unpack(f'<B3i{len(buff) - 14}sx', buff)
+    lon /= 100000
+    lat /= 100000
+    label = label.decode()
+    return lon, lat, label, status
